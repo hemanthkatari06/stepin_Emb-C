b16641aad6196f8fb76389ac6a3a09b8502731ab
hemanthkatari06 <89626453+hemanthkatari06@users.noreply.github.com>
2021-09-16 21:19:00 +0530

    Update activity4.h

--- 3_Implementation/Inc/activity4.h
+++ 3_Implementation/Inc/activity4.h
@@ -26,7 +26,7 @@
  * 
  * @param ubrr_value 
  */
-void USART_Init(uint16_t ubrr_value);
+void USART(uint16_t ubrr_value);
 
 /**
  * @brief write to usart
@@ -34,14 +34,7 @@ void USART_Init(uint16_t ubrr_value);
  * @param data 
  */
 
-void USART_WriteChar(int data);
-
-
-
-
-
-
-
+void USART_Write(int data);
 
 
 #endif
