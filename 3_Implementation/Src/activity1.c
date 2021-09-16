ad8dc0f7b2131fc26d4f411f089c1a8c0d9aeec7
hemanthkatari06 <89626453+hemanthkatari06@users.noreply.github.com>
2021-09-16 21:08:06 +0530

    Update activity1.c

--- 3_Implementation/Src/activity1.c
+++ 3_Implementation/Src/activity1.c
@@ -4,7 +4,7 @@
  * @brief function for declaring peripherals led,button,heater.
  * 
  */
-void peripheral_init(){
+void peripherals(){
 SET_LED_AS_OUTPUT;
  SET_BUTTON_AS_INPUT;
  SET_HEATER_AS_INPUT;
